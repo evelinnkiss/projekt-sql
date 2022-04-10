a82d55da0608cadeeeb416ea4b72fa51285da2a6
evelinnkiss <102673338+evelinnkiss@users.noreply.github.com>
2022-04-10 14:27:47 +0200

    Update 3_vyzkumni_otazka.sql

--- 3_vyzkumni_otazka.sql
+++ 3_vyzkumni_otazka.sql
@@ -1,13 +1,12 @@
 /* Najpomaleji zdražujou Banány žluté, mají nejnižší percentualni meziroční narůst, Cukr a jablka maji meziroční pokles */
-with 2018y as (
-	select distinct goods,year,average_goods_price  from t_dasa_kiss_project_sql_primary_final 
-	where year in ('2018')
-), 2006y as (
-	select distinct goods,year,average_goods_price  from t_dasa_kiss_project_sql_primary_final
-	where year in ('2006')	
+with maxYear as (
+	select distinct  goods, year, average_goods_price from t_dasa_kiss_project_sql_primary_final
+	where year= (select max(year) from t_dasa_kiss_project_sql_primary_final)
+), minYear as (
+	select distinct  goods, year, average_goods_price from t_dasa_kiss_project_sql_primary_final
+	where year= (select min(year) from t_dasa_kiss_project_sql_primary_final)	
 ) 
-select 2018y.*, 2006y.year, 2006y.average_goods_price, ROUND((POWER((2018y.average_goods_price /2006y.average_goods_price),(1/13))-1)*100,2) as percentage_increase  from 2018y
-join 2006y
-on 2018y.goods=2006y.goods
-order by percentage_increase
-
+select maxYear.*, minYear.year, minYear.average_goods_price, ROUND((POWER((maxYear.average_goods_price /minYear.average_goods_price),(1/13))-1)*100,2) as percentage_increase  from maxYear
+join minYear
+on maxYear.goods=minYear.goods
+order by percentage_increase 
