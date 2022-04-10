3282b01e0798153ca8d8d1f3c6915ff9c55151c8
evelinnkiss <102673338+evelinnkiss@users.noreply.github.com>
2022-04-10 14:01:43 +0200

    Update 3_vyzkumni_otazka.sql

--- 3_vyzkumni_otazka.sql
+++ 3_vyzkumni_otazka.sql
@@ -1,10 +1,10 @@
-/* Najpomaleji zdražujou Banány žluté, mají nejnižší percentualni meziroèní narùst, Cukr a jablka maji meziroèní pokles */
+/* Najpomaleji zdraÅ¾ujou BanÃ¡ny Å¾lutÃ©, majÃ­ nejniÅ¾Å¡Ã­ percentualni meziroÃ¨nÃ­ narÃ¹st, Cukr a jablka maji meziroÃ¨nÃ­ pokles */
 with 2018y as (
-	select distinct goods,year,average_goods_price  from t_dasa_kiss_project_sql_primary_final 
-	where year in ('2018')
+	select distinct goods, MAX(year) as year,average_goods_price  from t_dasa_kiss_project_sql_primary_final 
+	group by goods
 ), 2006y as (
-	select distinct goods,year,average_goods_price  from t_dasa_kiss_project_sql_primary_final
-	where year in ('2006')	
+	select distinct goods, MIN(year) as year,average_goods_price  from t_dasa_kiss_project_sql_primary_final 
+	group by goods	
 ) 
 select 2018y.*, 2006y.year, 2006y.average_goods_price, ROUND((POWER((2018y.average_goods_price /2006y.average_goods_price),(1/13))-1)*100,2) as percentage_increase  from 2018y
 join 2006y
