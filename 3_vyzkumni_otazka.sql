d5308985d47422075e954d903a56f5c25a1f5822
evelinnkiss <102673338+evelinnkiss@users.noreply.github.com>
2022-04-11 14:57:34 +0200

    Update 3_vyzkumni_otazka.sql

--- 3_vyzkumni_otazka.sql
+++ 3_vyzkumni_otazka.sql
@@ -1,12 +1,12 @@
 /* Najpomaleji zdražujou Banány žluté, mají nejnižší percentualni meziroční narůst, Cukr a jablka maji meziroční pokles */
 with maxYear as (
-	select distinct  goods, year, average_goods_price from t_dasa_kiss_project_sql_primary_final
+	select distinct  goods, year, average_goods_price as goods_price from t_dasa_kiss_project_sql_primary_final
 	where year= (select max(year) from t_dasa_kiss_project_sql_primary_final)
 ), minYear as (
-	select distinct  goods, year, average_goods_price from t_dasa_kiss_project_sql_primary_final
+	select distinct  goods, year, average_goods_price as goods_price from t_dasa_kiss_project_sql_primary_final
 	where year= (select min(year) from t_dasa_kiss_project_sql_primary_final)	
 ) 
-select maxYear.*, minYear.year, minYear.average_goods_price, ROUND((POWER((maxYear.average_goods_price /minYear.average_goods_price),(1/13))-1)*100,2) as percentage_increase  from maxYear
+select maxYear.*, minYear.year, minYear.goods_price, ROUND((POWER((maxYear.goods_price /minYear.goods_price),(1/13))-1)*100,2) as percentage_increase  from maxYear
 join minYear
 on maxYear.goods=minYear.goods
 order by percentage_increase 
