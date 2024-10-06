38fcd9bf6a917669b51d7b741a899473937f61c7
evelinnkiss <102673338+evelinnkiss@users.noreply.github.com>
2024-10-06 10:46:22 +0200

    Update 2_vyzkumni_otazka.sql

--- 2_vyzkumni_otazka.sql
+++ 2_vyzkumni_otazka.sql
@@ -1,37 +1,34 @@
 /* V privním období je možné koupít z prumerneho platu 1262 kg chlébu a 1409 litrov mléka
- *V posledním oddobí je možné koupít z prumerneho platu 1319 kg chlébu a 1614 litrov mléka */
+ * V posledním oddobí je možné koupít z prumerneho platu 1319 kg chlébu a 1614 litrov mléka */
 
-with  goods as (
-
-	select distinct 
-		year, 
-		goods,
-		average_goods_price as goods_price,
-		goods_value,
-		goods_unit 
-	from t_dasa_kiss_project_sql_primary_final
-	where goods  in ('Mléko polotuèné pasterované','Chléb konzumní kmínový')
-	and year in (
-		(select distinct MAX(year) from t_dasa_kiss_project_sql_primary_final),
-		(select distinct MIN(year) from t_dasa_kiss_project_sql_primary_final)
-	)
-
-), industries as(
-
-	select distinct 
-		year, 
-		AVG(average_payroll) over (partition by year)  as payroll
-	from t_dasa_kiss_project_sql_primary_final
-	where year in (
-		(select distinct MAX(year) from t_dasa_kiss_project_sql_primary_final),
-		(select distinct MIN(year) from t_dasa_kiss_project_sql_primary_final)
-	)
-
+WITH goods AS (
+    SELECT DISTINCT 
+        year, 
+        goods,
+        average_goods_price AS goods_price,
+        goods_value,
+        goods_unit 
+    FROM t_dasa_kiss_project_sql_primary_final
+    WHERE goods IN ('Mléko polotuèné pasterované', 'Chléb konzumní kmínový')
+	AND year IN (
+            (SELECT DISTINCT MAX(year) FROM t_dasa_kiss_project_sql_primary_final),
+            (SELECT DISTINCT MIN(year) FROM t_dasa_kiss_project_sql_primary_final)
+        )
+), 
+industries AS (
+    SELECT DISTINCT 
+        year, 
+        AVG(average_payroll) OVER (PARTITION BY year) AS payroll
+    FROM t_dasa_kiss_project_sql_primary_final
+    WHERE year IN (
+            (SELECT DISTINCT MAX(year) FROM t_dasa_kiss_project_sql_primary_final),
+            (SELECT DISTINCT MIN(year) FROM t_dasa_kiss_project_sql_primary_final)
+        )
 )
-select 
-	g.year,
-	g.goods,
-	ROUND((payroll/goods_price),0) as amount_of_goods,
-	g.goods_unit  
-from industries i 
-join goods g 						on i.year=g.year
+SELECT 
+    g.year,
+    g.goods,
+    ROUND((payroll / goods_price), 0) AS amount_of_goods,
+    g.goods_unit  
+FROM  industries i 
+JOIN goods g 						ON i.year = g.year;
