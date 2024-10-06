ab3c17e06b1c617b9abce416406014a8a3f5e95a
evelinnkiss <102673338+evelinnkiss@users.noreply.github.com>
2024-10-06 10:40:30 +0200

    Update 2_vyzkumni_otazka.sql

--- 2_vyzkumni_otazka.sql
+++ 2_vyzkumni_otazka.sql
@@ -1,19 +1,37 @@
-/* V privnÌm obdobÌ je moûnÈ koupÌt z prumerneho platu 1262 kg chlÈbu a 1409 litrov mlÈka
- *V poslednÌm oddobÌ je moûnÈ koupÌt z prumerneho platu 1319 kg chlÈbu a 1614 litrov mlÈka */
+/* V privn√≠m obdob√≠ je mo≈æn√© koup√≠t z prumerneho platu 1262 kg chl√©bu a 1409 litrov ml√©ka
+ *V posledn√≠m oddob√≠ je mo≈æn√© koup√≠t z prumerneho platu 1319 kg chl√©bu a 1614 litrov ml√©ka */
 
 with  goods as (
 
-	select distinct year, goods,average_goods_price as goods_price,goods_value, goods_unit  from t_dasa_kiss_project_sql_primary_final
-	where goods  in ('MlÈko polotuËnÈ pasterovanÈ','ChlÈb konzumnÌ kmÌnov˝')
-	and year in ((select distinct MAX(year) from t_dasa_kiss_project_sql_primary_final),
-	(select distinct MIN(year) from t_dasa_kiss_project_sql_primary_final))
+	select distinct 
+		year, 
+		goods,
+		average_goods_price as goods_price,
+		goods_value,
+		goods_unit 
+	from t_dasa_kiss_project_sql_primary_final
+	where goods  in ('Ml√©ko polotu√®n√© pasterovan√©','Chl√©b konzumn√≠ km√≠nov√Ω')
+	and year in (
+		(select distinct MAX(year) from t_dasa_kiss_project_sql_primary_final),
+		(select distinct MIN(year) from t_dasa_kiss_project_sql_primary_final)
+	)
 
 ), industries as(
 
-	select distinct year, AVG(average_payroll) over (partition by year)  as payroll  from t_dasa_kiss_project_sql_primary_final
-	where year in ((select distinct MAX(year) from t_dasa_kiss_project_sql_primary_final),
-	(select distinct MIN(year) from t_dasa_kiss_project_sql_primary_final))
+	select distinct 
+		year, 
+		AVG(average_payroll) over (partition by year)  as payroll
+	from t_dasa_kiss_project_sql_primary_final
+	where year in (
+		(select distinct MAX(year) from t_dasa_kiss_project_sql_primary_final),
+		(select distinct MIN(year) from t_dasa_kiss_project_sql_primary_final)
+	)
 
-)select g.year,g.goods,ROUND((payroll/goods_price),0) as amount_of_goods,g.goods_unit  from industries i 
-join goods g 
-on i.year=g.year
+)
+select 
+	g.year,
+	g.goods,
+	ROUND((payroll/goods_price),0) as amount_of_goods,
+	g.goods_unit  
+from industries i 
+join goods g 						on i.year=g.year
