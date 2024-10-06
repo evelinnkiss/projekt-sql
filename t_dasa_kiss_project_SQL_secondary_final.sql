4b223249eac3bc9e14a398e8c82c2714d727ad5a
evelinnkiss <102673338+evelinnkiss@users.noreply.github.com>
2024-10-06 10:53:04 +0200

    Update t_dasa_kiss_project_SQL_secondary_final.sql

--- t_dasa_kiss_project_SQL_secondary_final.sql
+++ t_dasa_kiss_project_SQL_secondary_final.sql
@@ -1,9 +1,15 @@
-create or replace table engeto_2022_02_03.t_dasa_kiss_project_SQL_secondary_final as (
-	select c.country, c.continent,e.year,e.GDP,e.gini,e.population,e.taxes  from countries c 
-	join economies e 
-	on c.country =e.country 
-	where year >2005
-	and year <2019
-	and continent ='Europe'
+CREATE OR REPLACE TABLE engeto_2022_02_03.t_dasa_kiss_project_SQL_secondary_final AS (
+    SELECT 
+        c.country, 
+        c.continent, 
+        e.year, 
+        e.GDP, 
+        e.gini, 
+        e.population, 
+        e.taxes  
+    FROM countries c 
+    JOIN economies e ON c.country = e.country 
+    WHERE year > 2005
+    AND year < 2019
+    AND continent = 'Europe'
 )
-
