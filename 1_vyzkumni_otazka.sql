faf29b2b6f4bbbad1c5b87bd3f06925586f36259
evelinnkiss <102673338+evelinnkiss@users.noreply.github.com>
2024-10-06 10:38:23 +0200

    Update 1_vyzkumni_otazka.sql

--- 1_vyzkumni_otazka.sql
+++ 1_vyzkumni_otazka.sql
@@ -1,6 +1,14 @@
-/* Mzdy v prubehu let rostou, maximalni mzda ve všech odvetvich je v poslednom zaznamenanom roku a tým je 2018*/
-select distinct industry_name, average_payroll as max_payroll ,year 
+/* Mzdy v prubehu let rostou, maximalni mzda ve vÅ¡ech odvetvich je v poslednom zaznamenanom roku a tÃ½m je 2018*/
+
+
+
+select distinct 
+	industry_name, 
+	average_payroll as max_payroll,
+	year 
 from t_dasa_kiss_project_SQL_primary_final
 where average_payroll in ( 
-	select distinct MAX(average_payroll) over (partition by industry_name) as max_payroll   from t_dasa_kiss_project_SQL_primary_final
+	select distinct 
+		MAX(average_payroll) over (partition by industry_name) as max_payroll
+	from t_dasa_kiss_project_SQL_primary_final
 	)
