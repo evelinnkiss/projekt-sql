e2b92eb3bb0fe26f2449ec0e45752140d3ef6a59
evelinnkiss <102673338+evelinnkiss@users.noreply.github.com>
2024-10-06 10:43:51 +0200

    Update 3_vyzkumni_otazka.sql

--- 3_vyzkumni_otazka.sql
+++ 3_vyzkumni_otazka.sql
@@ -1,12 +1,31 @@
 /* Najpomaleji zdražujou Banány žluté, mají nejnižší percentualni meziroční narůst, Cukr a jablka maji meziroční pokles */
-with maxYear as (
-	select distinct  goods, year, average_goods_price as goods_price from t_dasa_kiss_project_sql_primary_final
-	where year= (select max(year) from t_dasa_kiss_project_sql_primary_final)
-), minYear as (
-	select distinct  goods, year, average_goods_price as goods_price from t_dasa_kiss_project_sql_primary_final
-	where year= (select min(year) from t_dasa_kiss_project_sql_primary_final)	
+WITH maxYear AS (
+    SELECT DISTINCT  
+        goods, 
+        year, 
+        average_goods_price AS goods_price 
+    FROM t_dasa_kiss_project_sql_primary_final
+    WHERE year = (
+        SELECT MAX(year) 
+        FROM t_dasa_kiss_project_sql_primary_final
+    )
+), 
+minYear AS (
+    SELECT DISTINCT  
+        goods, 
+        year, 
+        average_goods_price AS goods_price 
+    FROM t_dasa_kiss_project_sql_primary_final
+    WHERE year = (
+        SELECT MIN(year) 
+        FROM t_dasa_kiss_project_sql_primary_final
+    )	
 ) 
-select maxYear.*, minYear.year, minYear.goods_price, ROUND((POWER((maxYear.goods_price /minYear.goods_price),(1/13))-1)*100,2) as percentage_increase  from maxYear
-join minYear
-on maxYear.goods=minYear.goods
-order by percentage_increase 
+SELECT 
+    maxYear.*, 
+    minYear.year AS min_year, 
+    minYear.goods_price AS min_goods_price, 
+    ROUND((POWER((maxYear.goods_price / minYear.goods_price), (1/13)) - 1) * 100, 2) AS percentage_increase  
+FROM maxYear
+JOIN minYear			ON maxYear.goods = minYear.goods
+ORDER BY percentage_increase;
